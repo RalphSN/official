245310b4cd2ddaa36829ff082d22e44f2f59ec35
RalphSN <ralph.sn.2025@gmail.com>
2025-02-14 15:28:10 +0800

    0214 service

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,21 @@
-import './App.css'
-import Header from './components/Header/Header'
-import Cover from './pages/Cover/Cover'
-import Service from './pages/Service/Service'
-import './styles/reset.css';
+import "./App.css";
+import Header from "./components/Header/Header";
+import Cover from "./pages/Cover/Cover";
+import Service from "./pages/Service/Service";
+import "./styles/reset.css";
 
 function App() {
-const title = "遊戲夢想實踐者"; 
-const description = "將創意化為現實，為您創造無與倫比的價值！";
+  const title = "遊戲夢想實踐者";
+  const description = "將創意化為現實，為您創造無與倫比的價值！";
   return (
     <>
-    <Header />
-    <Cover title={title} description={description} />
-    <Service/>
+      <Header />
+      <main className="main">
+        <Cover title={title} description={description} />
+        <Service />
+      </main>
     </>
-  )
+  );
 }
 
 export default App;
--- src/pages/Service/Service.jsx
+++ src/pages/Service/Service.jsx
@@ -1,27 +1,42 @@
 import "./Service.scss";
 
-const services = [
-  { key: "cust", label: "專業客服支援" },
-  { key: "sdk", label: "高效SDK整合" },
-  { key: "design", label: "創新軟體設計" },
-  { key: "develop", label: "定製遊戲開發" },
-  { key: "pub", label: "遊戲發行服務" },
-  { key: "opt", label: "平台設計優化" }
-];
+const baseUrl = "https://cdn.jsdelivr.net/gh/RalphSN/images@main/official-images/service-item";
+
+let services = [
+  { key: "cust", label: "專業客服支援", alt: "icon" },
+  { key: "sdk", label: "高效SDK整合", alt: "icon" },
+  { key: "design", label: "創新軟體設計", alt: "icon" },
+  { key: "develop", label: "定製遊戲開發", alt: "icon" },
+  { key: "pub", label: "遊戲發行服務", alt: "icon" },
+  { key: "opt", label: "平台設計優化", alt: "icon" }
+].map(service => ({
+  ...service,
+  url: `${baseUrl}/${service.key}.svg`
+}));
+
 
 const Service = () => {
   return (
     <section className="service">
       <h2 className="service-title">專屬於您的最佳選擇</h2>
       <div className="service-dots">
-        {services.map(service => (
-          <div key={service.key} className={`dot ${service.key}`} title={service.label}></div>
+        {services.map((service) => (
+          <div
+            key={service.key}
+            className={`dot ${service.key}`}
+            title={service.label}
+          ></div>
         ))}
       </div>
       <div className="service-item-container">
-        {services.map(service => (
+        {services.map((service) => (
           <div key={service.key} className={`service-item ${service.key}`}>
-            <span>{service.label}</span>
+            <div className="service-item-inner">
+              <div className="service-item-icon">
+                <img src={service.url} alt={service.icon} />
+              </div>
+              <span className={`service-word ${service.key}-word`}>{service.label}</span>
+            </div>
           </div>
         ))}
       </div>
@@ -30,4 +45,3 @@ const Service = () => {
 };
 
 export default Service;
-
